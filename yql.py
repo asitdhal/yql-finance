17a7225ee42949860c9781d8b82c94c1404a7ffb
Asit Dhal <lipun4u@gmail.com>
2013-01-07 16:31:36 +0530

    fetch.py not needed

--- yql.py
+++ yql.py
@@ -1,49 +1,16 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
+# Name:        fetch.py
+# Purpose:     Interacts with YQL server and fetched quotes
 # Author:      asit
-#
-# Created:
 # Copyright:   (c) asit
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
 import urllib2
-import urllib
-import types
-import json
+import httplib
+from threading import Thread
 from elementtree import ElementTree
-
-##http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22MSFT%22)&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env
-debug=False
-base_url = "http://query.yahooapis.com/v1/public/yql"
-append_url = "&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env"
-query = "select * from yahoo.finance.quotes where symbol in "
-format="xml"
-
-def fetch(url):
-    response = urllib2.urlopen(url)
-    if  debug == True:
-        print "Url : ", url
-        print "Response : ", response
-    response_data = response.read()
-    response.close()
-    return response_data
-
-def make_url(symbols):
-    symbol_url=""
-    if isinstance(symbols, types.ListType):
-        for symbol in symbols:
-            if len(symbol_url) == 0:
-                symbol_url ='\"' + symbol + '\"'
-            else:
-                symbol_url += ",\"" + symbol + '\"'
-    else:
-        symbol_url = '\"' + symbols + '\"'
-    encoded_url=urllib2.quote((query + "(" + symbol_url + ")").encode('utf8'))
-    final_url = base_url + "?q=" + encoded_url + append_url + "&format=" + format
-    return final_url
+import types
+import threading
 
 class XmlListConfig(list):
     def __init__(self, aList):
@@ -107,112 +74,88 @@ class XmlDictConfig(dict):
             else:
                 self.update({element.tag: element.text})
 
-def print_symbol(quote):
-    print 'YearLow' , " :: ",  quote[ 'YearLow' ]
-    print 'OneyrTargetPrice' , " :: ",  quote[ 'OneyrTargetPrice' ]
-    print 'DividendShare' , " :: ",  quote[ 'DividendShare' ]
-    print 'ChangeFromFiftydayMovingAverage' , " :: ",  quote[ 'ChangeFromFiftydayMovingAverage' ]
-    print 'ExDividendDate' , " :: ",  quote[ 'ExDividendDate' ]
-    print 'SharesOwned' , " :: ",  quote[ 'SharesOwned' ]
-    print 'PercentChangeFromTwoHundreddayMovingAverage' , " :: ",  quote[ 'PercentChangeFromTwoHundreddayMovingAverage' ]
-    print 'PricePaid' , " :: ",  quote[ 'PricePaid' ]
-    print 'DaysLow' , " :: ",  quote[ 'DaysLow' ]
-    print 'DividendYield' , " :: ",  quote[ 'DividendYield' ]
-    print 'Commission' , " :: ",  quote[ 'Commission' ]
-    print 'EPSEstimateNextQuarter' , " :: ",  quote[ 'EPSEstimateNextQuarter' ]
-    print 'HoldingsGainRealtime' , " :: ",  quote[ 'HoldingsGainRealtime' ]
-    print 'FiftydayMovingAverage' , " :: ",  quote[ 'FiftydayMovingAverage' ]
-    print 'EarningsShare' , " :: ",  quote[ 'EarningsShare' ]
-    print 'AverageDailyVolume' , " :: ",  quote[ 'AverageDailyVolume' ]
-    print 'LastTradePriceOnly' , " :: ",  quote[ 'LastTradePriceOnly' ]
-    print 'YearHigh' , " :: ",  quote[ 'YearHigh' ]
-    print 'EBITDA' , " :: ",  quote[ 'EBITDA' ]
-    print 'ErrorIndicationreturnedforsymbolchangedinvalid' , " :: ",  quote[ 'ErrorIndicationreturnedforsymbolchangedinvalid' ]
-    print 'AnnualizedGain' , " :: ",  quote[ 'AnnualizedGain' ]
-    print 'ShortRatio' , " :: ",  quote[ 'ShortRatio' ]
-    print 'LastTradeDate' , " :: ",  quote[ 'LastTradeDate' ]
-    print 'PriceSales' , " :: ",  quote[ 'PriceSales' ]
-    print 'EPSEstimateCurrentYear' , " :: ",  quote[ 'EPSEstimateCurrentYear' ]
-    print 'BookValue' , " :: ",  quote[ 'BookValue' ]
-    print 'Bid' , " :: ",  quote[ 'Bid' ]
-    print 'AskRealtime' , " :: ",  quote[ 'AskRealtime' ]
-    print 'PreviousClose' , " :: ",  quote[ 'PreviousClose' ]
-    print 'DaysRangeRealtime' , " :: ",  quote[ 'DaysRangeRealtime' ]
-    print 'EPSEstimateNextYear' , " :: ",  quote[ 'EPSEstimateNextYear' ]
-    print 'Volume' , " :: ",  quote[ 'Volume' ]
-    print 'HoldingsGainPercent' , " :: ",  quote[ 'HoldingsGainPercent' ]
-    print 'Change_PercentChange' , " :: ",  quote[ 'Change_PercentChange' ]
-    print 'PercentChange' , " :: ",  quote[ 'PercentChange' ]
-    print 'TickerTrend' , " :: ",  quote[ 'TickerTrend' ]
-    print 'Ask' , " :: ",  quote[ 'Ask' ]
-    print 'ChangeRealtime' , " :: ",  quote[ 'ChangeRealtime' ]
-    print 'PriceEPSEstimateNextYear' , " :: ",  quote[ 'PriceEPSEstimateNextYear' ]
-    print 'HoldingsGain' , " :: ",  quote[ 'HoldingsGain' ]
-    print 'Change' , " :: ",  quote[ 'Change' ]
-    print 'MarketCapitalization' , " :: ",  quote[ 'MarketCapitalization' ]
-    print 'Name' , " :: ",  quote[ 'Name' ]
-    print 'HoldingsValue' , " :: ",  quote[ 'HoldingsValue' ]
-    print 'DaysRange' , " :: ",  quote[ 'DaysRange' ]
-    print 'AfterHoursChangeRealtime' , " :: ",  quote[ 'AfterHoursChangeRealtime' ]
-    print 'symbol' , " :: ",  quote[ 'symbol' ]
-    print 'ChangePercentRealtime' , " :: ",  quote[ 'ChangePercentRealtime' ]
-    print 'DaysValueChange' , " :: ",  quote[ 'DaysValueChange' ]
-    print 'LastTradeTime' , " :: ",  quote[ 'LastTradeTime' ]
-    print 'StockExchange' , " :: ",  quote[ 'StockExchange' ]
-    print 'DividendPayDate' , " :: ",  quote[ 'DividendPayDate' ]
-    print 'LastTradeRealtimeWithTime' , " :: ",  quote[ 'LastTradeRealtimeWithTime' ]
-    print 'Notes' , " :: ",  quote[ 'Notes' ]
-    print 'MarketCapRealtime' , " :: ",  quote[ 'MarketCapRealtime' ]
-    print 'PERatio' , " :: ",  quote[ 'PERatio' ]
-    print 'DaysValueChangeRealtime' , " :: ",  quote[ 'DaysValueChangeRealtime' ]
-    print 'ChangeFromYearHigh' , " :: ",  quote[ 'ChangeFromYearHigh' ]
-    print 'ChangeinPercent' , " :: ",  quote[ 'ChangeinPercent' ]
-    print 'HoldingsValueRealtime' , " :: ",  quote[ 'HoldingsValueRealtime' ]
-    print 'PercentChangeFromFiftydayMovingAverage' , " :: ",  quote[ 'PercentChangeFromFiftydayMovingAverage' ]
-    print 'PEGRatio' , " :: ",  quote[ 'PEGRatio' ]
-    print 'ChangeFromTwoHundreddayMovingAverage' , " :: ",  quote[ 'ChangeFromTwoHundreddayMovingAverage' ]
-    print 'DaysHigh' , " :: ",  quote[ 'DaysHigh' ]
-    print 'PercentChangeFromYearLow' , " :: ",  quote[ 'PercentChangeFromYearLow' ]
-    print 'TradeDate' , " :: ",  quote[ 'TradeDate' ]
-    print 'LastTradeWithTime' , " :: ",  quote[ 'LastTradeWithTime' ]
-    print 'BidRealtime' , " :: ",  quote[ 'BidRealtime' ]
-    print 'YearRange' , " :: ",  quote[ 'YearRange' ]
-    print 'HighLimit' , " :: ",  quote[ 'HighLimit' ]
-    print 'OrderBookRealtime' , " :: ",  quote[ 'OrderBookRealtime' ]
-    print 'ChangeFromYearLow' , " :: ",  quote[ 'ChangeFromYearLow' ]
-    print 'PriceBook' , " :: ",  quote[ 'PriceBook' ]
-    print 'LowLimit' , " :: ",  quote[ 'LowLimit' ]
-    print 'HoldingsGainPercentRealtime' , " :: ",  quote[ 'HoldingsGainPercentRealtime' ]
-    print 'TwoHundreddayMovingAverage' , " :: ",  quote[ 'TwoHundreddayMovingAverage' ]
-    print 'PERatioRealtime' , " :: ",  quote[ 'PERatioRealtime' ]
-    print 'PercebtChangeFromYearHigh' , " :: ",  quote[ 'PercebtChangeFromYearHigh' ]
-    print 'Open' , " :: ",  quote[ 'Open' ]
-    print 'PriceEPSEstimateCurrentYear' , " :: ",  quote[ 'PriceEPSEstimateCurrentYear' ]
-    print 'MoreInfo' , " :: ",  quote[ 'MoreInfo' ]
 
+class UrlFetch(object):
 
-def parse_yql_response(data):
-    root = ElementTree.XML(data)
-    xmldict = XmlDictConfig(root)
-    for quote in xmldict["results"]["quote"]:
-        print "################################################################"
-        print_symbol(quote)
-        print "################################################################"
+    def __init__(self, url, debug=False):
+        self.response_data =''
+        try:
+            response = urllib2.urlopen(url)
+            if  debug == True:
+                print "Url : ", url
+                print "Response : ", response
+            self.response_data = response.read()
+            response.close()
+        except urllib2.URLError, e:
+            print "URL Error >> ", str(e.reason)
+        except urllib2.HTTPError, e:
+            print "HTTP Error >> ", str(e.code)
+        except httplib.HTTPException, e:
+            print "HTTP Exception"
+        except Exception:
+            import traceback
+            print traceback.format_exc()
 
+    def Get(self):
+        return self.response_data
 
+class Yql(object):
 
+    def __init__(self, symbols, fields='*'):
+        self.base_url = "http://query.yahooapis.com/v1/public/yql"
+        self.append_url = "&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env"
+        self.query = "select %s from yahoo.finance.quotes where symbol in "
+        self.format="xml"
+        self.url = ''
+        self.symbols = symbols
+        self.fields = fields
+        self.encoded_url = ''
+        self.res_data = ''
 
-##print fetch("http://kodeyard.blogspot.in")
-#print fetch("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22MSFT%22)&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env")
+    def _make_url(self):
+        field_str = ''
+        if isinstance(self.fields, types.ListType):
+            if 'Symbol' not in self.fields:
+                self.fields.append('Symbol')
+            for field in self.fields:
+                if len(field_str) == 0:
+                    field_str = field
+                else:
+                    field_str += ", " + field
+            self.query = self.query % (field_str)
+        elif self.fields == '*':
+            self.query = self.query % (self.fields)
+        else:
+            if self.fields != 'Symbol':
+                field_str = self.fields + ", " + 'Symbol'
+            else:
+                field_str = self.fields
+            self.query = self.query % (self.fields)
+        if isinstance(self.symbols, types.ListType):
+            for symbol in self.symbols:
+                if len(self.url) == 0:
+                    self.url ='\"' + symbol + '\"'
+                else:
+                    self.url += ",\"" + symbol + '\"'
+        else:
+            self.url = '\"' + self.symbols + '\"'
+        encoded_url=urllib2.quote((self.query + "(" + self.url + ")").encode('utf8'))
+        #print encoded_url
+        self.encoded_url= self.base_url + "?q=" + encoded_url + self.append_url + "&format=" + self.format
+        #print "**************************************"
+        #print self.encoded_url
 
-lis = []
-lis.append("TCS.NS")
-lis.append("HINDALCO.NS")
+    def query_yql(self):
+        self._make_url()
+        res = UrlFetch(self.encoded_url)
+        self.res_data = res.Get()
+        self.res_quotes = {}
+        root = ElementTree.XML(self.res_data)
+        xmldict = XmlDictConfig(root)
+        for quote in xmldict["results"]["quote"]:
+            self.res_quotes[quote['Symbol']] = quote
+        return self.res_quotes
 
-url = make_url(lis)
-##url = urllib2.quote(url.encode('utf8'))
-print url
-res = fetch(url)
-print res
-parse_yql_response(res)
+#class YqlWrapper(threading.Thread):
 
+#    def __init__(self, symbol, callback):
